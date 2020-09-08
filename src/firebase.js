a7255e0084b95441af01aa8d086e77c2c95bdc3d
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-08 21:39:31 +0000

    Initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAs8TEAJ4LaBZ9_Yxoh7znqeh-isUj_JpI',
+  authDomain: 'mo--clone.firebaseapp.com',
+  databaseURL: 'https://mo--clone.firebaseio.com',
+  projectId: 'mo--clone',
+  storageBucket: 'mo--clone.appspot.com',
+  messagingSenderId: '1078120764963',
+  appId: '1:1078120764963:web:6804d0d0eaa3a2a68c4396',
+};
