25e21ba8d50c7b078e973ff7475e139db74e7f05
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-09 19:40:58 +0000

    Add the lean startup picture

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Product.css';
+import lean from './lean_startup.jpg';
 
-function Product() {
-  return <div className="product"></div>;
+function Product({}) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <div className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </div>
+        <div className="product__rating">
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+        </div>
+      </div>
+      <img src={lean} alt="lean startup" />
+    </div>
+  );
 }
 
 export default Product;
