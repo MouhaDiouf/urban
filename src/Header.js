fff658446d9820512f212ad979f28ddbd10b66aa
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-09 18:41:34 +0000

    Style the navigation

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './amazon_logo.png';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
   return (
@@ -8,18 +9,19 @@ function Header() {
       <img src={logo} className="header__logo" alt="Logo" />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optoinLineOne">Hello Mouha</span>
+          <span className="header__optionLineOne">Hello Mouha</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
-          <span className="header__optoinLineOne">Returns</span>
+          <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo"> & Orders</span>
         </div>
         <div className="header__optin">
-          <span className="header__optoinLineOne">Your</span>
+          <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo"> Prime</span>
         </div>
       </div>
