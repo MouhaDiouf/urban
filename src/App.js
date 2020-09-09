df2d920ec5196ed0450676b09b42d67978bf2287
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-09 20:44:33 +0000

    Create the checkout component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>I am a checout page</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <h1>I am a checkout page!!</h1>
+    </div>
+  );
+}
+
+export default Checkout;
