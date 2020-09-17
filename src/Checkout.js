82626b6fad63e4826231004617f9f6371758fd3f
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-17 14:21:43 +0000

    Use useHistory to redirect to the payment page from subtotal

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,9 +3,10 @@ import './Checkout.css';
 import Subtotal from './Subtotal';
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
+import FlipMove from 'react-flip-move';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -14,7 +15,9 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+
         <div>
+          <h3>Hello {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           <div className="checkout__baskets">
             {basket.map((item, index) => (
